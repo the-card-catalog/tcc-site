927b6f66d1d17eef3d9ba28f7b48ef4bacb23d59
michael-masarik <masarikfamilymichael@gmail.com>
2026-07-17 20:11:35 -0500

    Added front gallery

--- src/components/Galleries.tsx
+++ src/components/Galleries.tsx
@@ -1,3 +1,4 @@
+import { Bookmark, BookMarked, CircleQuestionMark, Mail, Mic } from "lucide-react";
 import { ATLTags } from "./Tags";
 import { Card, CardContent, CardHeader } from "./ui/card";
 
@@ -23,4 +24,70 @@ export function ATLGallery({ articles }: { articles: Array<Article> }) {
 
         ))}
     </div>)
+}
+
+export function FrontGallery() {
+
+    return (<div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3  gap-4 py-4">
+        {/* ATL */}
+        <a className="link-wrapper" href="/ask-the-librarian">
+            <div className="bg-card rounded-lg  ring-1 ring-foreground/10 h-full ">
+                <div className="bg-primary/50 items-center flex justify-center rounded-t-lg p-4">
+                    <CircleQuestionMark size={50} className="text-primary h-15" />
+                </div>
+                <div className=" flex items-center justify-center h-22 p-4">
+                    <h2 className="mt-0 no-underline text-center">Ask the Librarian</h2>
+                </div>
+            </div>
+        </a>
+        {/* Shelf Notes */}
+        {/* TODO: Add real link */}
+        <a className="link-wrapper" href="">
+            <div className="bg-card rounded-lg  ring-1 ring-foreground/10 h-full ">
+                <div className="bg-primary/50 items-center flex justify-center rounded-t-lg p-4">
+                    <Mail size={50} className="text-primary h-15" />
+                </div>
+                <div className=" flex items-center justify-center h-22 p-4">
+                    <h2 className="mt-0 no-underline text-center">The Shelf Notes</h2>
+                </div>
+            </div>
+        </a>
+        {/* Librarian Stories */}
+        {/* TODO: Add real link */}
+        <a className="link-wrapper" href="">
+            <div className="bg-card rounded-lg  ring-1 ring-foreground/10 h-full ">
+                <div className="bg-primary/50 items-center flex justify-center rounded-t-lg p-4">
+                    <BookMarked size={50} className="text-primary h-15" />
+                </div>
+                <div className=" flex items-center justify-center h-22 p-4">
+                    <h2 className="mt-0 no-underline text-center">Librarian Stories</h2>
+                </div>
+            </div>
+        </a>
+        {/* Librarian Reflections */}
+        {/* TODO: Add real link */}
+        <a className="link-wrapper" href="">
+            <div className="bg-card rounded-lg  ring-1 ring-foreground/10 h-full ">
+                <div className="bg-primary/50 items-center flex justify-center rounded-t-lg p-4">
+                    <Bookmark size={50} className="text-primary h-15" />
+                </div>
+                <div className=" flex items-center justify-center h-22 p-4">
+                    <h2 className="mt-0 no-underline text-center">Librarian Reflections</h2>
+                </div>
+            </div>
+        </a>
+        {/* Libraries in the News */}
+        {/* TODO: Add real link */}
+        <a className="link-wrapper" href="">
+            <div className="bg-card rounded-lg  ring-1 ring-foreground/10 h-full ">
+                <div className="bg-primary/50 items-center flex justify-center rounded-t-lg p-4">
+                    <Mic size={50} className="text-primary h-15" />
+                </div>
+                <div className=" flex items-center justify-center h-22 p-4">
+                    <h2 className="mt-0 no-underline text-center">Libraries in the News</h2>
+                </div>
+            </div>
+        </a>
+    </div>)
+
 }
