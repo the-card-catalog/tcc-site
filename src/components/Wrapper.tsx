2cd97f42c922625e42f708fceebde8522bf5fe2f
michael-masarik <masarikfamilymichael@gmail.com>
2026-07-17 20:24:55 -0500

    Fixed ARIA compliance

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -3,9 +3,9 @@ import { Nav } from "./Nav";
 export function Wrapper({ children }: React.PropsWithChildren) {
     return (<>
         <Nav />
-        <div className="w-full sm:h-75 h-60 overflow-hidden">
+        <header role="banner" className="w-full sm:h-75 h-60 overflow-hidden">
             <img src="/banner.avif" className="object-cover w-full h-full" alt="An old card catalog" />
-        </div>
-        <div className="m-auto width-[90%] md:w-[80%] lg:w-[75%] typeset typeset-article bg-muted p-8">{children}</div>
+        </header>
+        <main role="main" className="m-auto width-[90%] md:w-[80%] lg:w-[75%] typeset typeset-article bg-muted p-8">{children}</main>
     </>)
 }
